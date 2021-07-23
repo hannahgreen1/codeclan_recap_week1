fefae796025b8ec9d7a41c407f60fedb90f5d45a
hannahgreen1 <hannahgreen94@icloud.com>
2021-07-23 11:36:22 +0100

    two passing tests - get cohort name works

--- src/recap.py
+++ src/recap.py
@@ -1,1 +1,3 @@
 # WRITE YOUR FUNCTIONS HERE
+def get_cohort_name(cohort):
+    return cohort["cohort_name"]
--- tests/recap_test.py
+++ tests/recap_test.py
@@ -61,11 +61,45 @@ class TestRecap(unittest.TestCase):
             "cohort_name": "CodeClan E51"
         }
 
+        self.cc_cohort2 = {
+            "students": [
+                {
+                    "name": "Amy",
+                    "favourite_language": "Python",
+                    "age": 28
+                },
+                 {
+                    "name": "Dave",
+                    "favourite_language": "JavaScript",
+                    "age": 32
+                },
+                 {
+                    "name": "James",
+                    "favourite_language": "Java",
+                    "age": 40
+                },
+                 {
+                    "name": "Mary",
+                    "favourite_language": "Ruby",
+                    "age": 25
+                }
+            ],
+            "staff": {
+                "total_staff": 52,
+                "instructors": "Colin, Hannah",
+            },
+            "cohort_name": "CodeClan E52"
+        }
+
     def test_cohort_name(self):
         name = get_cohort_name(self.cc_cohort)
         self.assertEqual("CodeClan E51", name)
 
-    @unittest.skip("delete this line to run the test")
+    def test_cohort_name(self):
+        name = get_cohort_name(self.cc_cohort2)
+        self.assertEqual("CodeClan E52", name)
+
+    # @unittest.skip("delete this line to run the test")
     def test_total_staff(self):
         sum = get_total_staff(self.cc_cohort)
         self.assertEqual(52, sum)
